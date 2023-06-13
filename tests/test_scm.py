a404ad13f1ab3f36bcf8eb76926adc6987050d4c
dingweidong_960828 <960828@git.nd.com>
2023-06-13 15:26:30 +0800

    test(scm): add testcase

---
+++ tests/test_scm.py
@@ -0,0 +1,32 @@
+import unittest
+
+from pySourceControl.scm import SCM
+
+
+class TestSCM(unittest.TestCase):
+    def test_init(self):
+        self.scm = SCM()
+
+    def test_checkout(self):
+        pass
+
+    def test_switch(self):
+        pass
+
+    def test_revert(self):
+        pass
+
+    def test_cleanup(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_add(self):
+        pass
+
+    def test_commit(self):
+        pass
+
+    def test_cleanup(self):
+        pass
